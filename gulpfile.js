81b3c82741b369c611cff21c7319cd8eb5a73304
johndoe@example.com <brunoalv.s@outlook.com>
2016-03-09 17:33:39 -0300

    Mudanças na estrutura do projeto visando a melhoria de performance.

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('sass', function() {
   return gulp.src('assets/sass/**.{sass,scss}')
     .pipe(sass({ onError: browserSync.notify }))
     .pipe(prefix({ browsers: ['last 2 versions'], }))
-    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(gulp.dest('assets/css'))
     .pipe(browserSync.reload({stream:true}))
 });
 
@@ -50,7 +50,7 @@ gulp.task('jade', function(){
 gulp.task('watch', function () {
     gulp.watch('assets/sass/**/*.{sass,scss}', ['sass']);
     gulp.watch('_jadefiles/*.jade', ['jade']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*', 'assets/css/*.css'], ['jekyll-rebuild']);
 });
 
 // Default task, running just `gulp` will compile the sass, jade, compile the jekyll site, launch BrowserSync & watch files.

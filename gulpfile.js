df6dfd030d6382389bc7782410e7a85645c82c67
johndoe@example.com <brunoalv.s@outlook.com>
2016-03-28 12:11:16 -0300

    Atualização nas páginas de portfolio.

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 });
 
 // Wait for jekyll-build, then launch the Server
-gulp.task('browser-sync', ['sass', 'jade', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['sass', 'jade','sassjobs', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
@@ -39,6 +39,16 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({stream:true}))
 });
 
+// SASS/SCSS for Jobs pages
+gulp.task('sassjobs', function() {
+  return gulp.src('jobs/css/**.{sass,scss}')
+    .pipe(sass({ onError: browserSync.notify }))
+    .pipe(prefix({ browsers: ['last 2 versions'], }))
+    .pipe(gulp.dest('jobs/css'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+
 // Travis building a gulp task for jade
 gulp.task('jade', function(){
     return gulp.src('_jadefiles/*.jade')
@@ -50,7 +60,8 @@ gulp.task('jade', function(){
 gulp.task('watch', function () {
     gulp.watch('assets/sass/**/*.{sass,scss}', ['sass']);
     gulp.watch('_jadefiles/*.jade', ['jade']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*', 'assets/css/*.css'], ['jekyll-rebuild']);
+    gulp.watch('jobs/css/**/*.{sass,scss}', ['sassjobs']);
+    gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*', 'assets/css/*.css', 'jobs/*.html', 'jobs/css/*.css'], ['jekyll-rebuild']);
 });
 
 // Default task, running just `gulp` will compile the sass, jade, compile the jekyll site, launch BrowserSync & watch files.

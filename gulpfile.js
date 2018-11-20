67940f63be2db6ae0f553c8be99838317e3bfc8c
Bruno Alves <brunoalv.s@outlook.com>
2018-11-20 11:08:30 -0200

    add tasks from minify images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-
+const size = require('gulp-size');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
 const del = require('del');
 
 function clean() {
@@ -13,6 +15,22 @@ function styles() {
     .pipe(gulp.dest('assets/css'));
 }
 
+function image() {
+  return gulp.src('src/images/**/*.{png,gif,jpg,jpeg}')
+    .pipe(newer('dist'))
+    .pipe(imagemin({
+      interlaced: true,
+      progressive: true,
+      optimizationLevel: 5,
+      svgoPlugin: [
+        { removeViewBox: true },
+      ],
+    }))
+    .pipe(size({ title: 'image minify:' }))
+    .pipe(gulp.dest('dist'));
+}
+
 // CommonJs `exports` module
 exports.clean = clean;
 exports.styles = styles;
+exports.image = image;
